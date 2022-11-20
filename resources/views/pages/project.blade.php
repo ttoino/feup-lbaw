2c7ebe99b03da90449590415015719470dc93b3d
pedronunes19 <pedromiguelmnunes@gmail.com>
2022-11-20 15:17:07 +0000

    update project board

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -7,18 +7,17 @@
         <div class="col-10">
             <div class="row">
                 @foreach ($project->taskGroups as $g)
-                    <div class='col border border-primary m-3 p-3 text-center'>
+                    <div class='col border border-secondary m-3 p-3 text-center d-flex flex-column'>
                         <p>{{ $g->name }}</p>
                         @foreach ($g->tasks as $t)
-                            <p class="border border-primary p-1">{{ $t->name }}
-                            </p> {{-- change this later to redirect to the task url (need to have taskID) --}}
+                            <a href="{{ url('/project/' . $id . '/task/' . $t->id)}}"
+                            class="border border-secondary p-1 mb-3">{{ $t->name }}</a> 
                         @endforeach
                         <a href="{{ url('/project/' . $id . '/task/new') }}"
-                            class="border border-primary p-1">&plus; Create task</a>
-                        {{-- change this later to route() once it's defined --}}
+                            class="border border-secondary p-1">&plus; Create task</a>
                     </div>
                 @endforeach
-                <div class='col border border-primary m-3 p-3 text-center'>
+                <div class='col border border-secondary m-3 p-3 text-center'>
                     <a href="{{ url('/project/' . $id) }}">&plus; Create group</a>
                     {{-- change referral when OpenApi is updated --}}
                 </div>
