132b7cd97d1c4451c94ca4017602e3468deac53d
Nuno Pereira <nunoafonso2002@gmail.com>
2022-12-10 19:18:00 +0000

    Added confirmation modal when leaving project

--- resources/views/pages/project/info.blade.php
+++ resources/views/pages/project/info.blade.php
@@ -23,10 +23,27 @@
                 @endif
             </section>
             <section class="flex-fill d-flex flex-row gap-3" style="max-width: 50%">
-                <form method="POST" action="{{route('project.leave', ['project' => $project])}}">
-                    @csrf
-                    <button class="submit col btn btn-outline-danger">Leave project</button>
-                </form>
+                <button class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#leave-project-modal">
+                    Leave project
+                </button>
+        
+                <div class="modal fade" id="leave-project-modal" tabindex="-1" aria-labelledby="leave-project-modal-label" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content modal-body gap-3 align-items-center">
+                            <h3 class="modal-title fs-5" id="leave-project-modal-label">
+                                Are you sure you want to leave this project?
+                            </h3>
+                            <div class="hstack gap-2 align-self-center">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                <form method="POST" action="{{route('project.leave', ['project' => $project])}}">
+                                    @csrf
+                                    <button class="submit col btn btn-outline-danger">Confirm</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 @if (Request::user()->id === $project->coordinator)
                     <a href="" class="col btn btn-outline-secondary">Archive project</a>
                 @endif
