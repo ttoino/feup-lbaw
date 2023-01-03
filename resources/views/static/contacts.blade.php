2f032a9cf641c1748e0c2736a156421ba3b7478e
Beatriz <bea.cruz2001@gmail.com>
2023-01-03 19:05:59 +0000

    Contacts page modified

--- resources/views/static/contacts.blade.php
+++ resources/views/static/contacts.blade.php
@@ -2,20 +2,27 @@
 @push('main-classes', 'flex-column align-items-center justify-content-center ')
 
 @section('content')
-<header class=static_page>
-  <h2><a href="/contact">Contact</a></h2>
-</header>
-<section class=static_page id="contact_page">
-  We are ready to help with whatever you need. Contact us:
-  @if (Auth::check())
-    <form class="contact">
-      <input class="form-control" style="width: 50em;" type="text" name="contact" placeholder="Message">
-      <input type="submit" class="btn btn-primary" value="Send">
-    </form>
-  @else
-    <br>
-    <a href="/login" style="color:#ff3333">Please login first</a>
-  @endif
 
-</section>
+<header class=static_pag>
+  <div class="container">
+    <h1 class="my-3">Contacts </h1>
+    
+        <h3></h3>
+        <p>
+          <b>Location:</b> <br>
+          Faculdade de Engenharia (FEUP)<br>Rua Dr. Roberto Frias<br>4200-465 PORTO<br>
+        </p>
+        <p><i class="fa fa-envelope"></i>
+          <abbr title="Email"></abbr><b>Email:</b> 
+          <br> Beatriz Maia da Silva Cruz: <a href="mailto:up201905517@edu.fe.up.pt">up201905517@edu.fe.up.pt </a>
+          <br> João António Semedo Pereira: <a href="mailto:up202007145@edu.fe.up.pt"> up202007145@edu.fe.up.pt </a>
+          <br> Nuno Afonso Anjos Pereira:  <a href="mailto:up202007865@edu.fe.up.pt"> up202007865@edu.fe.up.pt </a>
+          <br> Pedro Miguel Magalhães Nunes: <a href="mailto:up202004714@edu.fe.up.pt"> up202004714@edu.fe.up.pt </a>
+        </p>
+      
+  </div>
+    </header>
+
+
+ 
 @endsection
