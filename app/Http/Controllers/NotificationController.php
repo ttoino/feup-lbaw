898f8b120e171297a157251ae1cccd46d828f5ae
Nuno Pereira <nunoafonso2002@gmail.com>
2023-01-03 15:17:39 +0000

    Added route to mark notifications as read

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class NotificationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Notification  $notification
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Notification $notification) {
+
+        $this->authorize('view', $notification);
+
+        return new JsonResponse($notification);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Notification  $notification
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Notification $notification)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Notification  $notification
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Notification $notification)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Notification  $notification
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Notification $notification)
+    {
+        //
+    }
+
+    public function markAsRead(Request $request, Notification $notification) {
+
+        $this->authorize('markRead', $notification);
+
+        $notification->markAsRead();
+
+        return new JsonResponse($notification);
+    }
+}
